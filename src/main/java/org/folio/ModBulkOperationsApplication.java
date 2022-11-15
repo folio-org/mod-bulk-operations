aedb21a152d161360d1a5f4fd1d6464a4ae2cad8
siarhei-charniak <siarhei_charniak@epam.com>
2022-11-15 16:14:19 +0300

    MODBULKOPS-2 - Bulk operation DB schema

--- src/main/java/org/folio/ModBulkOperationsApplication.java
+++ src/main/java/org/folio/ModBulkOperationsApplication.java
@@ -1,11 +1,9 @@
 package org.folio;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ModBulkOperationsApplication {
 
   public static void main(String[] args) {
