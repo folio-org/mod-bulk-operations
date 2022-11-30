2a6f0e624dd4a014e26ab8b030572af4c92abb32
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2022-11-30 14:42:19 +0200

    MODBULKOPS-4 Created schemas instead of pojo

--- src/main/java/org/folio/bulkops/client/DataExportClient.java
+++ src/main/java/org/folio/bulkops/client/DataExportClient.java
@@ -1,6 +1,6 @@
 package org.folio.bulkops.client;
 
-import org.folio.bulkops.domain.pojo.Job;
+import org.folio.bulkops.domain.dto.Job;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
+
+import static java.util.Objects.nonNull;
 
 @FeignClient
 public interface DataExportClient {
@@ -22,6 +24,13 @@ public interface DataExportClient {
   @PostMapping(value = "bulk-edit/{jobId}/upload")
   String uploadFile(@PathVariable UUID jobId, @RequestBody MultipartFile file);
 
-  @PostMapping(value = "bulk-edit/{jobId}/start")
-  String startJob(@PathVariable UUID jobId);
+  //NOSONAR
+  default String deleteFile(UUID jobId) {
+    var job = getJob(jobId);
+    if (nonNull(job)) {
+      var files = job.getFiles();
+      // TODO
+    }
+    return null;
+  }
 }
--- src/test/java/org/folio/bulkops/client/DataExportClientTest.java
+++ src/test/java/org/folio/bulkops/client/DataExportClientTest.java
@@ -2,12 +2,8 @@ package org.folio.bulkops.client;
 
 import lombok.SneakyThrows;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.folio.bulkops.domain.pojo.Job;
-import org.folio.bulkops.domain.pojo.JobStatus;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
@@ -16,14 +12,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
 import java.util.UUID;
 
-import static java.util.Objects.isNull;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -64,38 +57,6 @@ class DataExportClientTest {
     var actualNumOfLines = dataExportClient.uploadFile(UUID.randomUUID(), readFile(USERS_UUIDS));
     var expectedNumOfLines = "5";
     assertEquals(expectedNumOfLines, actualNumOfLines);
-  }
-
-  @Test
-  void shouldStartJob() {
-    final Job newJob = new Job();
-    when(dataExportClient.getJob(any(UUID.class))).then(e -> newJob);
-    when(dataExportClient.startJob(any(UUID.class)))
-      .then(e -> {
-        var argumentCaptor = ArgumentCaptor.forClass(UUID.class);
-        verify(dataExportClient).startJob(argumentCaptor.capture());
-        var job = dataExportClient.getJob(argumentCaptor.getValue());
-        job.setStartTime(new Date());
-        job.setStatus(JobStatus.SCHEDULED);
-        return StringUtils.EMPTY;
-      });
-    when(dataExportClient.upsertJob(any(Job.class))).then(e -> {
-      if (isNull(newJob.getId())) {
-        newJob.setId(UUID.randomUUID());
-      }
-      var argumentCaptor = ArgumentCaptor.forClass(Job.class);
-      verify(dataExportClient).upsertJob(argumentCaptor.capture());
-      var capturedJob = argumentCaptor.getValue();
-      newJob.setStatus(capturedJob.getStatus());
-      newJob.setStartTime(capturedJob.getStartTime());
-      return capturedJob;
-    });
-    var upsertedJob = dataExportClient.upsertJob(newJob);
-    dataExportClient.startJob(upsertedJob.getId());
-    var actualStatus = dataExportClient.getJob(upsertedJob.getId()).getStatus();
-    var expectedStatus = JobStatus.SCHEDULED;
-    assertEquals(expectedStatus, actualStatus);
-    assertNotNull(newJob.getStartTime());
   }
 
   private MultipartFile readFile(String fileName) throws IOException {
