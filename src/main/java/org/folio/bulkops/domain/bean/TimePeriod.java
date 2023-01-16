2310888ae3c99dc015708c04322dee9e1001244e
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-01-16 22:02:42 +0100

    MODBULKOPS-21: Use folio TimePeriod bean, not awssdk TimePeriod bean
    org.folio.bulkops.domain.bean.ServicePoint uses
    
    software.amazon.awssdk.services.budgets.model.TimePeriod
    
    It should use
    
    org.folio.bulkops.domain.bean.TimePeriod.

---
+++ src/main/java/org/folio/bulkops/domain/bean/TimePeriod.java
@@ -0,0 +1,44 @@
+package org.folio.bulkops.domain.bean;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@With
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimePeriod {
+  @JsonProperty("duration")
+  private int duration;
+
+  @JsonProperty("intervalId")
+  private String intervalId;
+
+  public ChronoUnit getInterval() {
+    final String id = getIntervalId();
+    if (id != null) {
+      return ChronoUnit.valueOf(id.toUpperCase());
+    } else {
+      // Default is days
+      return ChronoUnit.DAYS;
+    }
+  }
+
+  public long between(ZonedDateTime start, ZonedDateTime end) {
+    return getInterval().between(start, end);
+  }
+
+  public boolean isLongTermPeriod() {
+    final ChronoUnit chronoUnit = getInterval();
+
+    return chronoUnit == ChronoUnit.DAYS || chronoUnit == ChronoUnit.WEEKS
+      || chronoUnit == ChronoUnit.MONTHS;
+  }
+}
