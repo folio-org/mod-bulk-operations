ccb9014758dd6bc257a48ca04ced18bc443bacde
siarhei-charniak <siarhei_charniak@epam.com>
2023-01-23 13:23:46 +0300

    MODBULKOPS-11 - BulkOperationController (#27)

--- src/main/java/org/folio/bulkops/client/BulkEditClient.java
+++ src/main/java/org/folio/bulkops/client/BulkEditClient.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.folio.bulkops.configs.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "bulk-edit", configuration = FeignClientConfiguration.class)
 public interface BulkEditClient {
-  @PostMapping(value = "/{jobId}/upload")
+  @PostMapping(value = "/{jobId}/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   String uploadFile(@PathVariable UUID jobId, @RequestBody MultipartFile file);
 
   @PostMapping(value = "/{jobId}/start")
