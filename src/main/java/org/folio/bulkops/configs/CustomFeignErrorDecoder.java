053ede1d9e8022f563cb0de3755c8013ed02652e
siarhei-charniak <siarhei_charniak@epam.com>
2023-02-06 16:51:21 +0300

    error message improvements

--- src/main/java/org/folio/bulkops/configs/CustomFeignErrorDecoder.java
+++ src/main/java/org/folio/bulkops/configs/CustomFeignErrorDecoder.java
@@ -10,15 +10,22 @@ import org.springframework.http.HttpStatus;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class CustomFeignErrorDecoder implements ErrorDecoder {
 
   @Override
   public Exception decode(String methodKey, Response response) {
     String requestUrl = response.request().url();
     if (HttpStatus.NOT_FOUND.value() == response.status()) {
-      return new NotFoundException(requestUrl);
+      return new NotFoundException("Not found: " + requestUrl);
     } else if (HttpStatus.BAD_REQUEST.value() == response.status()) {
-      return new BadRequestException(requestUrl);
+      try (var bodyIs = response.body().asInputStream()) {
+        return new BadRequestException("Bad request: " + requestUrl + ", message: " + Arrays.toString(bodyIs.readAllBytes()));
+      } catch (IOException e) {
+        return new BadRequestException("Bad request: " + requestUrl);
+      }
     } else if (HttpStatus.INTERNAL_SERVER_ERROR.value() == response.status()) {
       return new ServerErrorException(requestUrl);
     }
