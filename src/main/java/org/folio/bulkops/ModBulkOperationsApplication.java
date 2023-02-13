fcde29ef5eb94b91034dd1dc60b4bb49e0fd5582
viachaslau_khandramai <Viachaslau_Khandramai@epam.com>
2023-02-13 09:40:25 +0100

    MODBULKOPS-36 - 10K items failed problem

--- src/main/java/org/folio/bulkops/ModBulkOperationsApplication.java
+++ src/main/java/org/folio/bulkops/ModBulkOperationsApplication.java
@@ -4,12 +4,10 @@ import org.folio.bulkops.configs.RemoteRepositoryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCaching
 @EnableConfigurationProperties(RemoteRepositoryConfig.class)
 public class ModBulkOperationsApplication {
 
