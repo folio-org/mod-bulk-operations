78eae4369d1de9499b416602aa26cdbfc33f1ce7
viachaslau_khandramai <Viachaslau_Khandramai@epam.com>
2023-02-16 17:50:54 +0100

    MODBULKOPS-40

--- src/main/java/org/folio/bulkops/domain/converter/CallNumberTypeConverter.java
+++ src/main/java/org/folio/bulkops/domain/converter/CallNumberTypeConverter.java
@@ -3,18 +3,10 @@ package org.folio.bulkops.domain.converter;
 import com.opencsv.bean.AbstractBeanField;
 import com.opencsv.exceptions.CsvConstraintViolationException;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import org.apache.commons.lang3.ObjectUtils;
-import org.folio.bulkops.domain.bean.EffectiveCallNumberComponents;
-import org.folio.bulkops.domain.format.SpecialCharacterEscaper;
 import org.folio.bulkops.service.HoldingsReferenceService;
-import org.folio.bulkops.service.ItemReferenceService;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import static com.github.jknack.handlebars.internal.lang3.ObjectUtils.isEmpty;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
-import static org.folio.bulkops.util.Constants.ARRAY_DELIMITER;
-import static org.folio.bulkops.util.Utils.ofEmptyString;
 
 public class CallNumberTypeConverter extends AbstractBeanField<String, String> {
 
@@ -25,15 +17,6 @@ public class CallNumberTypeConverter extends AbstractBeanField<String, String> {
 
   @Override
   protected String convertToWrite(Object value) {
-    if (ObjectUtils.isEmpty((value))) {
-      return EMPTY;
-    }
-    var components = (EffectiveCallNumberComponents) value;
-    List<String> entries = new ArrayList<>();
-    ofEmptyString(components.getCallNumber()).ifPresent(e -> entries.add(SpecialCharacterEscaper.escape(e)));
-    ofEmptyString(components.getPrefix()).ifPresent(e -> entries.add(SpecialCharacterEscaper.escape(e)));
-    ofEmptyString(components.getSuffix()).ifPresent(e -> entries.add(SpecialCharacterEscaper.escape(e)));
-    entries.add(SpecialCharacterEscaper.escape(ItemReferenceService.service().getCallNumberTypeNameById(components.getTypeId())));
-    return String.join(ARRAY_DELIMITER, entries);
+    return isEmpty(value) ? EMPTY : HoldingsReferenceService.service().getCallNumberTypeNameById(value.toString());
   }
 }
