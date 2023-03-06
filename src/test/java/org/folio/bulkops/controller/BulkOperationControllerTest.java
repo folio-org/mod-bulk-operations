65278ef06ecb3e97b5f0082c98126140158a43f0
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2023-03-06 18:17:45 +0200

    MODBULKOPS-30 Added test

--- src/test/java/org/folio/bulkops/controller/BulkOperationControllerTest.java
+++ src/test/java/org/folio/bulkops/controller/BulkOperationControllerTest.java
@@ -1,23 +1,34 @@
 package org.folio.bulkops.controller;
 
+import org.folio.bulkops.BaseTest;
+import org.folio.bulkops.client.RemoteFileSystemClient;
+import org.folio.bulkops.domain.dto.FileContentType;
+import org.folio.bulkops.domain.entity.BulkOperation;
+import org.folio.bulkops.repository.BulkOperationRepository;
+import org.folio.bulkops.service.BulkOperationService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
 import static java.lang.String.format;
+import static org.folio.bulkops.domain.dto.EntityType.USER;
+import static org.folio.bulkops.domain.dto.IdentifierType.BARCODE;
+import static org.folio.bulkops.domain.dto.OperationStatusType.NEW;
+import static org.folio.bulkops.domain.dto.OperationType.UPDATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.io.InputStream;
-import java.util.UUID;
-
-import org.folio.bulkops.BaseTest;
-import org.folio.bulkops.client.RemoteFileSystemClient;
-import org.folio.bulkops.domain.dto.FileContentType;
-import org.folio.bulkops.domain.entity.BulkOperation;
-import org.folio.bulkops.service.BulkOperationService;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 class BulkOperationControllerTest extends BaseTest {
 
@@ -25,6 +36,9 @@ class BulkOperationControllerTest extends BaseTest {
   private BulkOperationService bulkOperationService;
   @MockBean
   private RemoteFileSystemClient remoteFileSystemClient;
+
+  @Autowired
+  private BulkOperationRepository bulkOperationRepository;
 
   @ParameterizedTest
   @EnumSource(value = FileContentType.class)
@@ -50,5 +64,34 @@ class BulkOperationControllerTest extends BaseTest {
         .headers(defaultHeaders())
         .contentType(APPLICATION_JSON))
       .andExpect(status().isOk());
+  }
+
+  @Test
+  void shouldHaveHrIdWhenGetBulkOperationCollection() throws Exception {
+    IntStream.range(0, 5).forEach(i -> bulkOperationRepository.save(BulkOperation.builder()
+      .id(UUID.randomUUID())
+      .userId(UUID.randomUUID())
+      .operationType(UPDATE)
+      .entityType(USER)
+      .identifierType(BARCODE)
+      .status(NEW)
+      .dataExportJobId(UUID.randomUUID())
+      .totalNumOfRecords(1)
+      .processedNumOfRecords(0)
+      .executionChunkSize(5)
+      .startTime(LocalDateTime.now())
+      .build()));
+    mockMvc.perform(get("/bulk-operations?query=(entityType==\"USER\") sortby startTime/sort.descending&limit=50&offset=0")
+        .headers(defaultHeaders())
+        .contentType(APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(result -> {
+        var bulkOperations = new JSONObject(result.getResponse().getContentAsString()).getJSONArray("bulkOperations");
+        assertEquals(5, bulkOperations.length());
+        for (int i = 0; i < bulkOperations.length(); i++) {
+          var bulkOperation = bulkOperations.getJSONObject(i);
+          assertEquals(bulkOperations.length() - i, bulkOperation.getInt("hrId"));
+        }
+      });
   }
 }
