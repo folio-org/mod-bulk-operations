6ca89cd021ad99c5a86d80ae436e8cdcaf3108fc
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-03-15 16:10:51 +0300

    MODBULKOPS-67 - error if entity equals to updated

--- src/main/java/org/folio/bulkops/processor/AbstractDataProcessor.java
+++ src/main/java/org/folio/bulkops/processor/AbstractDataProcessor.java
@@ -52,7 +52,7 @@ public abstract class AbstractDataProcessor<T extends BulkOperationsEntity> impl
     if (countErrors < countActions) {
       holder.setShouldBeUpdated(true);
     }
-    if (compare(updated, entity)) {
+    if (compare(updated, entity) && holder.shouldBeUpdated) {
       errorService.saveError(rules.getBulkOperationRules().get(0).getBulkOperationId(), identifier, "No change in value required");
       holder.setShouldBeUpdated(false);
     }
