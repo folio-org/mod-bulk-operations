0a5170a97edd94c36e0c4a79c44a96484657afdb
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-03-16 10:00:14 +0300

    MODBULKOPS-67 - remove unused import

--- src/main/java/org/folio/bulkops/service/ErrorService.java
+++ src/main/java/org/folio/bulkops/service/ErrorService.java
@@ -12,7 +12,6 @@ import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
