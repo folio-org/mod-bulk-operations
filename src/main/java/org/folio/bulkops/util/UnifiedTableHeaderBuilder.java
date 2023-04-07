a61c087c1e7a397eb101aa027ba71a561bccc031
siarhei-charniak <siarhei_charniak@epam.com>
2023-04-07 13:56:36 +0300

    MODBULKOPS-66 - TD - BulkOps: UnifiedTable header should be reflected from csv models (#83)

--- src/main/java/org/folio/bulkops/util/UnifiedTableHeaderBuilder.java
+++ src/main/java/org/folio/bulkops/util/UnifiedTableHeaderBuilder.java
@@ -3,13 +3,19 @@ package org.folio.bulkops.util;
 import static org.folio.bulkops.domain.dto.DataType.STRING;
 
 import com.opencsv.bean.CsvCustomBindByName;
+import com.opencsv.bean.CsvCustomBindByPosition;
+import com.opencsv.bean.CsvRecurse;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.folio.bulkops.domain.bean.BulkOperationsEntity;
 import org.folio.bulkops.domain.dto.Cell;
 import org.folio.bulkops.domain.dto.UnifiedTable;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @UtilityClass
 public class UnifiedTableHeaderBuilder {
@@ -18,8 +24,18 @@ public class UnifiedTableHeaderBuilder {
   }
 
   public static List<Cell> getHeaders(Class<? extends BulkOperationsEntity> clazz) {
-    return FieldUtils.getFieldsListWithAnnotation(clazz, CsvCustomBindByName.class).stream()
-      .map(field -> new Cell().dataType(STRING).value(field.getAnnotation(CsvCustomBindByName.class).column()).visible(true))
-      .toList();
+    return Stream.concat(
+        FieldUtils.getFieldsListWithAnnotation(clazz, CsvRecurse.class).stream()
+          .map(Field::getType)
+          .map(aClass -> FieldUtils.getFieldsListWithAnnotation(aClass, CsvCustomBindByName.class))
+          .flatMap(List::stream),
+        FieldUtils.getFieldsListWithAnnotation(clazz, CsvCustomBindByName.class).stream())
+      .collect(Collectors.toMap(field -> field.getAnnotation(CsvCustomBindByPosition.class).position(),
+        field -> field.getAnnotation(CsvCustomBindByName.class).column(),
+        (key1, key2) -> key1,
+        TreeMap::new))
+      .values().stream()
+        .map(val -> new Cell().dataType(STRING).value(val).visible(true))
+        .toList();
   }
 }
