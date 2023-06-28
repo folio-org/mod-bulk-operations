e1ce1896cec1abb1c56204ab9e620b3b04b29cb4
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2023-06-28 15:35:18 +0400

    MODBULKOPS-102 - update items logic

--- src/main/java/org/folio/bulkops/processor/HoldingsUpdateProcessor.java
+++ src/main/java/org/folio/bulkops/processor/HoldingsUpdateProcessor.java
@@ -37,7 +37,7 @@ public class HoldingsUpdateProcessor implements UpdateProcessor<HoldingsRecord> 
       var items = itemClient.getByQuery(String.format(GET_ITEMS_BY_HOLDING_ID_QUERY, holdingsRecord.getId()))
         .getItems();
       items.forEach(item -> {
-        if (item.getDiscoverySuppress() != holdingsRecord.getDiscoverySuppress()) {
+        if (item.getDiscoverySuppress() == null || !item.getDiscoverySuppress().equals(holdingsRecord.getDiscoverySuppress())) {
           item.setDiscoverySuppress(holdingsRecord.getDiscoverySuppress());
           try {
             itemClient.updateItem(item, item.getId());
