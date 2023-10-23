e6b8b6096cf4b245f850953bad904e93e2c34076
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2023-10-23 21:09:39 +0300

    MODBULKOPS-148 Added test

---
+++ src/test/java/org/folio/bulkops/domain/converter/DepartmentsConverterTest.java
@@ -0,0 +1,29 @@
+package org.folio.bulkops.domain.converter;
+
+import org.folio.bulkops.BaseTest;
+import org.folio.bulkops.domain.bean.Department;
+import org.folio.bulkops.domain.bean.DepartmentCollection;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.UUID;
+
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.Mockito.when;
+
+class DepartmentsConverterTest extends BaseTest {
+
+  @Test
+  void convertToObjectTestWhenTwoDepartments() {
+    UUID uuid1 = UUID.randomUUID();
+    UUID uuid2 = UUID.randomUUID();
+    when(departmentClient.getByQuery("name==\"dep1\"")).thenReturn(DepartmentCollection.builder()
+        .departments(singletonList(Department.builder().id(uuid1.toString()).build())).build());
+    when(departmentClient.getByQuery("name==\"dep2\"")).thenReturn(DepartmentCollection.builder()
+        .departments(singletonList(Department.builder().id(uuid2.toString()).build())).build());
+    DepartmentsConverter converter = new DepartmentsConverter();
+    var actual = converter.convertToObject("dep1;dep2");
+    assertIterableEquals(Set.of(uuid1, uuid2), actual);
+  }
+}
