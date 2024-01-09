2d5cf629ecf6ac2f31e12569c7e621a859d3a0ba
siarhei-charniak <siarhei_charniak@epam.com>
2024-01-09 13:59:26 +0300

    minor improvements

--- src/main/java/org/folio/bulkops/domain/converter/ContributorListConverter.java
+++ src/main/java/org/folio/bulkops/domain/converter/ContributorListConverter.java
@@ -1,6 +1,7 @@
 package org.folio.bulkops.domain.converter;
 
 import static org.folio.bulkops.util.Constants.ARRAY_DELIMITER;
+import static org.folio.bulkops.util.Constants.ARRAY_DELIMITER_SPACED;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +16,7 @@ public class ContributorListConverter extends BaseConverter<List<ContributorName
   @Override
   public List<ContributorName> convertToObject(String value) {
     return Arrays.stream(value.split(ARRAY_DELIMITER))
+      .map(String::trim)
       .map(SpecialCharacterEscaper::restore)
       .map(new ContributorName()::withName)
       .toList();
@@ -26,6 +28,6 @@ public class ContributorListConverter extends BaseConverter<List<ContributorName
       .filter(Objects::nonNull)
       .map(ContributorName::getName)
       .map(SpecialCharacterEscaper::escape)
-      .collect(Collectors.joining(ARRAY_DELIMITER));
+      .collect(Collectors.joining(ARRAY_DELIMITER_SPACED));
   }
 }
