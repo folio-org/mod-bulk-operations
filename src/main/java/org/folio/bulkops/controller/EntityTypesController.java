bcae653445488331a681fd41d28be0113e543ccc
Dmytro_Bykov <dmytro_bykov@epam.com>
2024-01-15 16:23:59 +0500

    MODBULKOPS-32: FQM Query Tool Metadata supporting. Initial commit

---
+++ src/main/java/org/folio/bulkops/controller/EntityTypesController.java
@@ -0,0 +1,7 @@
+package org.folio.bulkops.controller;
+
+import org.folio.querytool.rest.resource.EntityTypesApi;
+
+public class EntityTypesController implements EntityTypesApi {
+
+}
---
+++ src/main/java/org/folio/bulkops/controller/FqlQueryController.java
@@ -0,0 +1,6 @@
+package org.folio.bulkops.controller;
+
+import org.folio.querytool.rest.resource.FqlQueryApi;
+
+public class FqlQueryController implements FqlQueryApi {
+}
