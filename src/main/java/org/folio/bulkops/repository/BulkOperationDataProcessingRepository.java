35c6bab3ac8561705cdd52b0f0a9c9368ad92586
Antony Hruschev <Antony_Hruschev@epam.com>
2024-05-24 09:14:34 +0400

    MODBULKOPS-281: fixed tests. removed unused code.

--- src/main/java/org/folio/bulkops/repository/BulkOperationDataProcessingRepository.java
+++ src/main/java/org/folio/bulkops/repository/BulkOperationDataProcessingRepository.java
@@ -1,13 +1,11 @@
 package org.folio.bulkops.repository;
 
-import java.util.Optional;
 import java.util.UUID;
-
 import org.folio.bulkops.domain.entity.BulkOperationDataProcessing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BulkOperationDataProcessingRepository extends JpaRepository<BulkOperationDataProcessing, UUID> {
-  Optional<BulkOperationDataProcessing> findByBulkOperationId(UUID uuid);
+
 }
