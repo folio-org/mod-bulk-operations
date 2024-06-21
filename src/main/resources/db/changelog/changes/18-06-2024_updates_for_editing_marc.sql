44e9ab4a3ec2f97792a927411c1280c06d254c30
siarhei-charniak <siarhei_charniak@epam.com>
2024-06-21 18:17:09 +0300

    minor improvements

--- src/main/resources/db/changelog/changes/18-06-2024_updates_for_editing_marc.sql
+++ src/main/resources/db/changelog/changes/18-06-2024_updates_for_editing_marc.sql
@@ -12,7 +12,6 @@ CREATE TABLE IF NOT EXISTS bulk_operation_marc_rule (
   constraint fk_marc_rule_to_operation foreign key (bulk_operation_id)
    references bulk_operation(id) ON DELETE CASCADE);
 
-ALTER TYPE EntityType RENAME VALUE 'INSTANCE' TO 'INSTANCE_FOLIO';
 ALTER TYPE EntityType ADD VALUE 'INSTANCE_MARC';
 
 ALTER TYPE UpdateActionType ADD VALUE 'APPEND';
