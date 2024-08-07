7b0d362eddf02e12df396d53d094016d313f022d
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-08-07 17:00:43 +0400

    MODBULKOPS-308 - ERROR_NOT_DOWNLOAD_ORIGIN_FILE_FROM_S3

--- src/main/java/org/folio/bulkops/util/ErrorCode.java
+++ src/main/java/org/folio/bulkops/util/ErrorCode.java
@@ -2,7 +2,8 @@ package org.folio.bulkops.util;
 
 public class ErrorCode {
 
-  private ErrorCode() {};
+  private ErrorCode() {}
 
   public static final String ERROR_NOT_UPLOAD_FILE_S3_INVALID_CONFIGURATION = "error.mod-bulk-operations.not.upload.file.s3.invalid.configuration";
+  public static final String ERROR_NOT_DOWNLOAD_ORIGIN_FILE_FROM_S3 = "error.mod-bulk-operations.not.download.file.from.s3";
 }
