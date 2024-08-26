eac69204cd639ea62d4680431c201c342c98b612
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-08-26 10:32:50 +0300

    MODBULKOPS-323 - fix constant

--- src/main/java/org/folio/bulkops/util/DateHelper.java
+++ src/main/java/org/folio/bulkops/util/DateHelper.java
@@ -8,10 +8,11 @@ public class DateHelper {
   private DateHelper() {}
 
   private static final String MARC_DATE_TIME_PATTERN = "yyyyMMddHHmmss.SSS";
+  private static final int REMOVE_MILLISECONDS_CHARACTERS_COUNT = 2;
 
   public static String getDateTimeForMarc(Date date) {
     var marcDateFormatter = new SimpleDateFormat(MARC_DATE_TIME_PATTERN);
     var dateAsStr = marcDateFormatter.format(date);
-    return dateAsStr.substring(0, dateAsStr.length() - 2);
+    return dateAsStr.substring(0, dateAsStr.length() - REMOVE_MILLISECONDS_CHARACTERS_COUNT);
   }
 }
