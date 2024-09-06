2127d0f6ffb201a0df509921fa48baccec7e34ee
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-09-06 11:47:51 +0300

    MODBULKOPS-329 Test fixes

--- src/main/java/org/folio/bulkops/domain/bean/ConsortiumHoldingCollection.java
+++ src/main/java/org/folio/bulkops/domain/bean/ConsortiumHoldingCollection.java
@@ -6,10 +6,14 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.With;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@With
+@Builder(toBuilder = true)
 public class ConsortiumHoldingCollection {
 
   @JsonProperty("holdings")
--- src/main/java/org/folio/bulkops/domain/bean/ConsortiumItem.java
+++ src/main/java/org/folio/bulkops/domain/bean/ConsortiumItem.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.With;
+import lombok.Builder;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@With
+@Builder(toBuilder = true)
 public class ConsortiumItem {
   private String id;
   private String tenantId;
