7e4d48522e183a6d6a3ac59731c94802015ba129
siarhei-charniak <siarhei_charniak@epam.com>
2024-10-08 06:52:27 +0300

    MODBULKOPS-249 * minor fixes

--- src/main/java/org/folio/bulkops/service/MetadataProviderService.java
+++ src/main/java/org/folio/bulkops/service/MetadataProviderService.java
@@ -1,6 +1,7 @@
 package org.folio.bulkops.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.folio.bulkops.client.MetadataProviderClient;
 import org.folio.bulkops.domain.dto.DataImportJobExecution;
 import org.folio.bulkops.exception.NotFoundException;
@@ -10,12 +11,14 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class MetadataProviderService {
   private static final String COMPOSITE_PARENT = "COMPOSITE_PARENT";
   private final MetadataProviderClient metadataProviderClient;
 
   public DataImportJobExecution getDataImportJobExecutionByJobProfileId(UUID dataImportJobProfileId) {
     var executions = metadataProviderClient.getJobExecutionsByJobProfileIdAndSubordinationType(dataImportJobProfileId, COMPOSITE_PARENT);
+    log.info("executions: {}", executions);
     if (executions.getJobExecutions().isEmpty()) {
       throw new NotFoundException("Job execution not found by jobProfileId " + dataImportJobProfileId);
     }
