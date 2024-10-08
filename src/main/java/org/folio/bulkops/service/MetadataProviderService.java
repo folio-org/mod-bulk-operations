1d6623d8fb59431e6b0fe6944a25b5803d4e98a2
siarhei-charniak <siarhei_charniak@epam.com>
2024-10-08 15:41:22 +0300

    MODBULKOPS-249 * minor fixes

--- src/main/java/org/folio/bulkops/service/MetadataProviderService.java
+++ src/main/java/org/folio/bulkops/service/MetadataProviderService.java
@@ -3,7 +3,6 @@ package org.folio.bulkops.service;
 import static java.util.Objects.nonNull;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.folio.bulkops.client.MetadataProviderClient;
 import org.folio.bulkops.domain.dto.DataImportJobExecution;
 import org.folio.bulkops.domain.dto.DataImportProgress;
@@ -13,7 +12,6 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Log4j2
 public class MetadataProviderService {
   private static final String COMPOSITE_PARENT = "COMPOSITE_PARENT";
   private final MetadataProviderClient metadataProviderClient;
@@ -24,7 +22,6 @@ public class MetadataProviderService {
         .filter(jobExecution -> nonNull(jobExecution.getJobProfileInfo()))
         .filter(jobExecution -> jobExecution.getJobProfileInfo().getId().equals(dataImportJobProfileId))
         .toList();
-    log.info("executions: {}", executions);
 
     return executions.isEmpty() ?
       new DataImportJobExecution().progress(new DataImportProgress().current(0).total(0)) :
