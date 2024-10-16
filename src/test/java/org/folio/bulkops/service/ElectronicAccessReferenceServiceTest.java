2d5630187ecdf8e4305c967342b5f11913c2d7cf
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-10-16 09:54:11 +0300

    MODBULKOPS-334 Fixed test

--- src/test/java/org/folio/bulkops/service/ElectronicAccessReferenceServiceTest.java
+++ src/test/java/org/folio/bulkops/service/ElectronicAccessReferenceServiceTest.java
@@ -9,17 +9,13 @@ import org.folio.bulkops.domain.bean.ElectronicAccessRelationship;
 import org.folio.bulkops.domain.bean.ElectronicAccessRelationshipCollection;
 import org.folio.bulkops.exception.NotFoundException;
 import org.folio.spring.FolioExecutionContext;
-import org.folio.spring.integration.XOkapiHeaders;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class ElectronicAccessReferenceServiceTest {
@@ -34,9 +30,6 @@ class ElectronicAccessReferenceServiceTest {
   void shouldReturnRelationshipNameById() {
     var id = "id";
     var expectedName = "name";
-    HashMap<String, Collection<String>> headers = new HashMap<>();
-    headers.put(XOkapiHeaders.TENANT, List.of("diku"));
-    when(folioExecutionContext.getOkapiHeaders()).thenReturn(headers);
     when(electronicAccessRelationshipClient.getById(id)).thenReturn(new ElectronicAccessRelationship().withName(expectedName));
 
     var actualName = electronicAccessReferenceService.getRelationshipNameById(id, "diku");
@@ -48,9 +41,7 @@ class ElectronicAccessReferenceServiceTest {
   void shouldReturnIdIfRelationshipNotFound() {
     var id = "id";
     var expectedName = id;
-    HashMap<String, Collection<String>> headers = new HashMap<>();
-    headers.put(XOkapiHeaders.TENANT, List.of("diku"));
-    when(folioExecutionContext.getOkapiHeaders()).thenReturn(headers);
+
     when(electronicAccessRelationshipClient.getById(id)).thenThrow(new NotFoundException("Not found"));
 
     var actualName = electronicAccessReferenceService.getRelationshipNameById(id, "tenant");
