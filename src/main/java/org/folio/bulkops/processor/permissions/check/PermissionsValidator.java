6e7bff7f06f0437399feeb30e2648191dbf0cc28
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-10-18 15:28:30 +0300

    MODBULKOPS-384 - use enum as permissions

--- src/main/java/org/folio/bulkops/processor/permissions/check/PermissionsValidator.java
+++ src/main/java/org/folio/bulkops/processor/permissions/check/PermissionsValidator.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_INVENTORY_WRITE_PERMISSION;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_USERS_WRITE_PERMISSION;
+
 @Component
 @RequiredArgsConstructor
 @Log4j2
 public class PermissionsValidator {
-
-  public static final String BULK_EDIT_INVENTORY_WRITE_PERMISSION = "bulk-operations.item.inventory.put";
-  public static final String BULK_EDIT_USERS_WRITE_PERMISSION = "bulk-operations.item.users.put";
 
   private final PermissionsProvider permissionsProvider;
   private final RequiredPermissionResolver requiredPermissionResolver;
@@ -31,9 +31,9 @@ public class PermissionsValidator {
     var userPermissions = permissionsProvider.getUserPermissions(tenantId, folioExecutionContext.getUserId());
     var isWritePermissionsExist = false;
     if (entityType == EntityType.USER) {
-      isWritePermissionsExist = userPermissions.contains(readPermissionForEntity) && userPermissions.contains(BULK_EDIT_USERS_WRITE_PERMISSION);
+      isWritePermissionsExist = userPermissions.contains(readPermissionForEntity.getValue()) && userPermissions.contains(BULK_EDIT_USERS_WRITE_PERMISSION.getValue());
     } else {
-      isWritePermissionsExist = userPermissions.contains(readPermissionForEntity) && userPermissions.contains(BULK_EDIT_INVENTORY_WRITE_PERMISSION);
+      isWritePermissionsExist = userPermissions.contains(readPermissionForEntity.getValue()) && userPermissions.contains(BULK_EDIT_INVENTORY_WRITE_PERMISSION.getValue());
     }
     log.info("isBulkEditWritePermissionExists:: user {} has write permissions {} for {} in tenant {}", folioExecutionContext.getUserId(),
       isWritePermissionsExist, entityType, tenantId);
--- src/test/java/org/folio/bulkops/processor/permissions/check/RequiredPermissionResolverTest.java
+++ src/test/java/org/folio/bulkops/processor/permissions/check/RequiredPermissionResolverTest.java
@@ -10,9 +10,9 @@ class RequiredPermissionResolverTest {
   @Test
   void testGetWritePermission() {
     var requiredPermissionResolver = new RequiredPermissionResolver();
-    assertEquals("users.item.put",  requiredPermissionResolver.getWritePermission(EntityType.USER));
-    assertEquals("inventory.items.item.put",  requiredPermissionResolver.getWritePermission(EntityType.ITEM));
-    assertEquals("inventory-storage.holdings.item.put",  requiredPermissionResolver.getWritePermission(EntityType.HOLDINGS_RECORD));
-    assertEquals("inventory.instances.item.put",  requiredPermissionResolver.getWritePermission(EntityType.INSTANCE));
+    assertEquals("users.item.put",  requiredPermissionResolver.getWritePermission(EntityType.USER).getValue());
+    assertEquals("inventory.items.item.put",  requiredPermissionResolver.getWritePermission(EntityType.ITEM).getValue());
+    assertEquals("inventory-storage.holdings.item.put",  requiredPermissionResolver.getWritePermission(EntityType.HOLDINGS_RECORD).getValue());
+    assertEquals("inventory.instances.item.put",  requiredPermissionResolver.getWritePermission(EntityType.INSTANCE).getValue());
   }
 }
