ae6bb0e18edee0b6469e5c9121b480df8b9304fe
Aliaksei Harbuz <Aliaksei_Harbuz@epam.com>
2024-10-22 15:34:52 +0300

    MODBULKOPS-384 - Support Eureka permissions model for bulk operations (write operations) (#286)
    * MODBULKOPS-384 - use enum as permissions
    
    * MODBULKOPS-384 - support eureka permission model
    
    * MODBULKOPS-384 - fix variable name

--- src/main/java/org/folio/bulkops/processor/permissions/check/RequiredPermissionResolver.java
+++ src/main/java/org/folio/bulkops/processor/permissions/check/RequiredPermissionResolver.java
@@ -3,15 +3,20 @@ package org.folio.bulkops.processor.permissions.check;
 import org.folio.bulkops.domain.dto.EntityType;
 import org.springframework.stereotype.Component;
 
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_INSTANCES_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_ITEMS_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_STORAGE_HOLDINGS_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.USERS_ITEM_PUT;
+
 @Component
 public class RequiredPermissionResolver {
 
-  public String getWritePermission(EntityType entityType) {
+  public PermissionEnum getWritePermission(EntityType entityType) {
     return switch (entityType) {
-      case USER -> "users.item.put";
-      case ITEM -> "inventory.items.item.put";
-      case HOLDINGS_RECORD -> "inventory-storage.holdings.item.put";
-      case INSTANCE, INSTANCE_MARC -> "inventory.instances.item.put";
+      case USER -> USERS_ITEM_PUT;
+      case ITEM -> INVENTORY_ITEMS_ITEM_PUT;
+      case HOLDINGS_RECORD -> INVENTORY_STORAGE_HOLDINGS_ITEM_PUT;
+      case INSTANCE, INSTANCE_MARC -> INVENTORY_INSTANCES_ITEM_PUT;
     };
   }
 }
---
+++ src/main/java/org/folio/bulkops/service/UserPermissionsService.java
@@ -0,0 +1,53 @@
+package org.folio.bulkops.service;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.folio.bulkops.client.EurekaUserPermissionsClient;
+import org.folio.bulkops.client.OkapiUserPermissionsClient;
+import org.folio.spring.FolioExecutionContext;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_INVENTORY_WRITE_PERMISSION;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_USERS_WRITE_PERMISSION;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_INSTANCES_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_ITEMS_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.INVENTORY_STORAGE_HOLDINGS_ITEM_PUT;
+import static org.folio.bulkops.processor.permissions.check.PermissionEnum.USERS_ITEM_PUT;
+
+
+@RequiredArgsConstructor
+@Log4j2
+@Service
+public class UserPermissionsService {
+
+  public static final String EUREKA_PLATFORM = "eureka";
+  public static final String OKAPI_PLATFORM = "okapi";
+
+  @Setter
+  @Value("${application.platform}")
+  private String platform;
+
+  private final FolioExecutionContext folioExecutionContext;
+  private final OkapiUserPermissionsClient okapiUserPermissionsClient;
+  private final EurekaUserPermissionsClient eurekaUserPermissionsClient;
+
+  public List<String> getPermissions() {
+    if (StringUtils.equals(EUREKA_PLATFORM, platform)) {
+      var desiredPermissions = getDesiredPermissions();
+      return eurekaUserPermissionsClient.getPermissions(folioExecutionContext.getUserId().toString(),
+        desiredPermissions).getPermissions();
+    }
+    return okapiUserPermissionsClient.getPermissions(folioExecutionContext.getUserId().toString()).getPermissionNames();
+  }
+
+  private List<String> getDesiredPermissions() {
+    return List.of(BULK_EDIT_INVENTORY_WRITE_PERMISSION.getValue(), BULK_EDIT_USERS_WRITE_PERMISSION.getValue(),
+      USERS_ITEM_PUT.getValue(), INVENTORY_ITEMS_ITEM_PUT.getValue(), INVENTORY_STORAGE_HOLDINGS_ITEM_PUT.getValue(), INVENTORY_INSTANCES_ITEM_PUT.getValue());
+  }
+}
