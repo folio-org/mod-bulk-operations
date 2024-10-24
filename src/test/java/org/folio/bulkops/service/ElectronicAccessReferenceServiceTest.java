1cf9a48013ff574455300828448ec26c8e1a6a85
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2024-10-24 11:57:36 +0300

    MODBULKOPS-334 Fixed test

--- src/test/java/org/folio/bulkops/service/ElectronicAccessReferenceServiceTest.java
+++ src/test/java/org/folio/bulkops/service/ElectronicAccessReferenceServiceTest.java
@@ -9,13 +9,17 @@ import org.folio.bulkops.domain.bean.ElectronicAccessRelationship;
 import org.folio.bulkops.domain.bean.ElectronicAccessRelationshipCollection;
 import org.folio.bulkops.exception.NotFoundException;
 import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.integration.XOkapiHeaders;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class ElectronicAccessReferenceServiceTest {
@@ -28,8 +32,11 @@ class ElectronicAccessReferenceServiceTest {
 
   @Test
   void shouldReturnRelationshipNameById() {
-    var id = "id";
+    var id = "id;diku";
     var expectedName = "name";
+    HashMap<String, Collection<String>> headers = new HashMap<>();
+    headers.put(XOkapiHeaders.TENANT, List.of("tenant"));
+    when(folioExecutionContext.getOkapiHeaders()).thenReturn(headers);
     when(electronicAccessRelationshipClient.getById(id)).thenReturn(new ElectronicAccessRelationship().withName(expectedName));
 
     var actualName = electronicAccessReferenceService.getRelationshipNameById(id, "diku");
@@ -39,10 +46,13 @@ class ElectronicAccessReferenceServiceTest {
 
   @Test
   void shouldReturnIdIfRelationshipNotFound() {
-    var id = "id";
+    var id = "id;tenant";
     var expectedName = id;
 
+    HashMap<String, Collection<String>> headers = new HashMap<>();
+    headers.put(XOkapiHeaders.TENANT, List.of("tenant"));
     when(electronicAccessRelationshipClient.getById(id)).thenThrow(new NotFoundException("Not found"));
+    when(folioExecutionContext.getOkapiHeaders()).thenReturn(headers);
 
     var actualName = electronicAccessReferenceService.getRelationshipNameById(id, "tenant");
 
