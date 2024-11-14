f38e8042f29ac1daf0fc23f6d7214f0e6347ce9b
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2024-11-14 16:08:18 +0100

    MODBULKOPS-322 - Specific case of downloading empty file with matched Holdings records (#310)
     MODBULKOPS-322 - Specific case of downloading empty file with matched Holdings records

--- src/main/java/org/folio/bulkops/util/CSVHelper.java
+++ src/main/java/org/folio/bulkops/util/CSVHelper.java
@@ -2,18 +2,16 @@ package org.folio.bulkops.util;
 
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class CSVHelper {
   private static final char ASCII_ZERO_CHAR = '\0';
-  private static CSVParser csvParser;
+  @Getter
+  private static final CSVParser csvParser;
 
   static {
     csvParser = new CSVParserBuilder().withEscapeChar(ASCII_ZERO_CHAR).build();
-  }
-
-  public static CSVParser getCsvParser() {
-    return csvParser;
   }
 }
