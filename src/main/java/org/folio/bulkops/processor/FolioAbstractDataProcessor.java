9ededdff0eb1622d779ffd1c2c4089806a229b38
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2024-12-03 14:57:13 +0300

    MODBULKOPS-398 - fix sonar issues

--- src/main/java/org/folio/bulkops/processor/FolioAbstractDataProcessor.java
+++ src/main/java/org/folio/bulkops/processor/FolioAbstractDataProcessor.java
@@ -17,14 +17,12 @@ import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
 import org.folio.spring.scope.FolioExecutionContextSetter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import lombok.extern.log4j.Log4j2;
 
 import static org.folio.bulkops.util.FolioExecutionContextUtil.prepareContextForTenant;
 
 @Log4j2
-@Component
 public abstract class FolioAbstractDataProcessor<T extends BulkOperationsEntity> implements FolioDataProcessor<T> {
   private ErrorService errorService;
   protected FolioModuleMetadata folioModuleMetadata;
