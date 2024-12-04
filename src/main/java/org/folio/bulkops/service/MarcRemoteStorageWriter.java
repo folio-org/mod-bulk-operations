0e4e7a87d1df40fffdae8bdbc673c29e79f2506a
Viachaslau_Khandramai <Viachaslau_Khandramai@epam.com>
2024-12-04 18:23:18 +0300

    MODBULKOPS-433 - Optimisation of memory utilization

--- src/main/java/org/folio/bulkops/service/MarcRemoteStorageWriter.java
+++ src/main/java/org/folio/bulkops/service/MarcRemoteStorageWriter.java
@@ -1,30 +1,36 @@
 package org.folio.bulkops.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.folio.s3.client.FolioS3Client;
-import org.folio.s3.client.RemoteStorageWriter;
 import org.marc4j.MarcStreamWriter;
 import org.marc4j.marc.Record;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Log4j2
-public class MarcRemoteStorageWriter extends RemoteStorageWriter {
+public class MarcRemoteStorageWriter extends StringWriter {
 
-  public MarcRemoteStorageWriter(String path, int size, FolioS3Client s3Client) {
-    super(path, size, s3Client);
+  private final Writer remoteStorageWriter;
+  private final ByteArrayOutputStream byteArrayOutputStream;
+  private final MarcStreamWriter marcStreamWriter;
+
+  public MarcRemoteStorageWriter(Writer remoteStorageWriter) {
+    this.remoteStorageWriter = remoteStorageWriter;
+    byteArrayOutputStream = new ByteArrayOutputStream();
+    marcStreamWriter = new MarcStreamWriter(byteArrayOutputStream, StandardCharsets.UTF_8.name());
   }
 
-  public void writeRecord(Record marcRecord) throws IOException {
-    try (var byteArrayOutputStream = new ByteArrayOutputStream()) {
-      var marcStreamWriter = new MarcStreamWriter(byteArrayOutputStream, StandardCharsets.UTF_8.name());
-      marcStreamWriter.write(marcRecord);
-      super.write(byteArrayOutputStream.toString());
-    } catch (IOException e) {
-      log.error(e.getMessage());
-      throw e;
-    }
+  public void writeRecord(Record record) throws IOException {
+    marcStreamWriter.write(record);
+    remoteStorageWriter.write(byteArrayOutputStream.toString(StandardCharsets.UTF_8));
+    byteArrayOutputStream.reset();
+  }
+
+  public void close() throws IOException {
+    marcStreamWriter.close();
+    remoteStorageWriter.close();
   }
 }
