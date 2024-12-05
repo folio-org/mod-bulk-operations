c3de79cca7f496b04dc6f878953098f30d530765
Mikita Siadykh <mikita_siadykh@epam.com>
2024-12-05 16:38:05 +0300

    EUREKA-559 Errors on commit stage in Bulk edit when update Instances and apply changes to Holdings, Items on ECS bugfest Eureka environment (#326)

--- src/main/java/org/folio/bulkops/processor/permissions/check/PermissionsValidator.java
+++ src/main/java/org/folio/bulkops/processor/permissions/check/PermissionsValidator.java
@@ -6,14 +6,12 @@ import org.folio.spring.FolioExecutionContext;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_INVENTORY_WRITE_PERMISSION;
 import static org.folio.bulkops.processor.permissions.check.PermissionEnum.BULK_EDIT_USERS_WRITE_PERMISSION;
 
 @Component
 @RequiredArgsConstructor
-@Log4j2
 public class PermissionsValidator {
 
   private final PermissionsProvider permissionsProvider;
@@ -35,8 +33,6 @@ public class PermissionsValidator {
     } else {
       isWritePermissionsExist = userPermissions.contains(writePermissionForEntity.getValue()) && userPermissions.contains(BULK_EDIT_INVENTORY_WRITE_PERMISSION.getValue());
     }
-    log.info("isBulkEditWritePermissionExists:: user {} has write permissions {} for {} in tenant {}", folioExecutionContext.getUserId(),
-      isWritePermissionsExist, entityType, tenantId);
     return isWritePermissionsExist;
   }
 
