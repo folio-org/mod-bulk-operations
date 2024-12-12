ea1464935a01f287f71b954c6112c35bfd0ff9ff
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2024-12-12 15:14:58 +0200

    MODBULKOPS-389 Fixed test

--- src/main/java/org/folio/bulkops/util/Constants.java
+++ src/main/java/org/folio/bulkops/util/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
 
   public static final int ITEM_NOTE_POSITION = 31;
   public static final int HOLDINGS_NOTE_POSITION = 24;
-  public static final int INSTANCE_NOTE_POSITION = 22;
+  public static final int INSTANCE_NOTE_POSITION = 23;
 
   public static final String ITEM_DELIMITER_PATTERN = "\\|";
   public static final String KEY_VALUE_DELIMITER = ":";
