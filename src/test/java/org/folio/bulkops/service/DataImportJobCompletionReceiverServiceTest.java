8f83d527e83000713a28564155ea82fa625dfeed
Viachaslau_Khandramai <Viachaslau_Khandramai@epam.com>
2025-01-26 17:16:51 +0100

    MODBULKOPS-454 - Issue with data-import.splitconfig.get

--- src/test/java/org/folio/bulkops/service/DataImportJobCompletionReceiverServiceTest.java
+++ src/test/java/org/folio/bulkops/service/DataImportJobCompletionReceiverServiceTest.java
@@ -1,12 +1,16 @@
 package org.folio.bulkops.service;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.awaitility.Awaitility;
 import org.folio.bulkops.BaseTest;
 import org.folio.bulkops.configs.kafka.dto.Event;
+import org.folio.spring.model.SystemUser;
 import org.folio.spring.scope.FolioExecutionContextSetter;
+import org.folio.spring.service.SystemUserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,16 +34,19 @@ class DataImportJobCompletionReceiverServiceTest extends BaseTest {
   @MockBean
   private BulkOperationService bulkOperationService;
 
+  @MockBean
+  private SystemUserService systemUserService;
+
   @Test
   void testReceiveJobExecutionUpdate() throws IOException {
     try (var context = new FolioExecutionContextSetter(folioExecutionContext)) {
+      when(systemUserService.getAuthedSystemUser(any())).thenReturn(new SystemUser("mod-bulk-operation", "http://okapi:9130", "diku", null, ""));
       var topic = "folio.Default.diku.DI_JOB_COMPLETED";
       var event = Files.readString(Path.of("src/test/resources/files/kafka/data_import_job_completed_message.json"));
-      kafkaTemplate.send(topic, OBJECT_MAPPER.readValue(event, Event.class)).thenAccept(result -> {
-        var dataImportJobProfileIdCaptor = ArgumentCaptor.forClass(UUID.class);
-        Awaitility.await().atMost(20, TimeUnit.SECONDS).untilAsserted(() -> verify(bulkOperationService, times(1))
-          .processDataImportResult(dataImportJobProfileIdCaptor.capture()));
-      });
+      kafkaTemplate.send(topic, OBJECT_MAPPER.readValue(event, Event.class));
+      var dataImportJobProfileIdCaptor = ArgumentCaptor.forClass(UUID.class);
+      Awaitility.await().atMost(20, TimeUnit.SECONDS).untilAsserted(() -> verify(bulkOperationService, times(1))
+        .processDataImportResult(dataImportJobProfileIdCaptor.capture()));
     }
   }
 }
