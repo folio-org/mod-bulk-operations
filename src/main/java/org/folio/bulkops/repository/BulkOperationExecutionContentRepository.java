caedd2546bcfdcede8459d58d6f4f328148dd4b6
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2025-01-29 14:58:53 +0100

    MODBULKOPS-451 - Rework errors preview (#353)
    * MODBULKOPS-451 - Rework errors preview
    
    * Merge branch 'master' into MODBULKOPS-451
    
    # Conflicts:
    #	src/main/java/org/folio/bulkops/controller/BulkOperationController.java
    #	src/main/java/org/folio/bulkops/service/ErrorService.java
    #	src/main/resources/swagger.api/bulk-operations.yaml
    #	src/test/java/org/folio/bulkops/service/ErrorServiceTest.java
    
    * MODBULKOPS-451 - Rework errors preview
    
    * MODBULKOPS-451 - Rework errors preview
    
    * MODBULKOPS-451 - Rework errors preview

--- src/main/java/org/folio/bulkops/repository/BulkOperationExecutionContentRepository.java
+++ src/main/java/org/folio/bulkops/repository/BulkOperationExecutionContentRepository.java
@@ -8,6 +8,7 @@ import org.folio.bulkops.domain.entity.BulkOperationExecutionContent;
 import org.folio.spring.data.OffsetRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,6 +17,7 @@ public interface BulkOperationExecutionContentRepository extends JpaRepository<B
   Page<BulkOperationExecutionContent> findByBulkOperationIdAndErrorMessageIsNotNullAndErrorTypeIsOrderByErrorType(UUID bulkOperationId, OffsetRequest offsetRequest, ErrorType errorType);
   Optional<BulkOperationExecutionContent> findFirstByBulkOperationIdAndIdentifier(UUID bulkOperationId, String identifier);
   int countAllByBulkOperationIdAndErrorMessageIsNotNullAndErrorTypeIs(UUID bulkOperationId, ErrorType errorType);
-
+  @Query("SELECT COUNT(i) FROM BulkOperationExecutionContent i WHERE i.bulkOperationId = :bulkOperationId")
+  long countByBulkOperationId(UUID bulkOperationId);
   void deleteByBulkOperationId(UUID bulkOperationId);
 }
