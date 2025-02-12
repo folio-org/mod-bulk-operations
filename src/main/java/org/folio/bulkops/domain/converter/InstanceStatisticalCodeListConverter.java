13700763121fd848369b800a8c076c21d00d4fb8
siarhei-charniak <siarhei_charniak@epam.com>
2025-02-12 15:10:12 +0300

    minor improvements

--- src/main/java/org/folio/bulkops/domain/converter/InstanceStatisticalCodeListConverter.java
+++ src/main/java/org/folio/bulkops/domain/converter/InstanceStatisticalCodeListConverter.java
@@ -1,5 +1,7 @@
 package org.folio.bulkops.domain.converter;
 
+import static org.folio.bulkops.domain.format.SpecialCharacterEscaper.escape;
+
 import org.folio.bulkops.domain.format.SpecialCharacterEscaper;
 import org.folio.bulkops.service.InstanceReferenceHelper;
 
@@ -26,7 +28,7 @@ public class InstanceStatisticalCodeListConverter extends BaseConverter<List<Str
       .map(id -> {
         var sc = InstanceReferenceHelper.service().getStatisticalCodeById(id);
         var sct = InstanceReferenceHelper.service().getStatisticalCodeTypeById(sc.getStatisticalCodeTypeId());
-        return String.format("%s: %s - %s", (sct.getName()), sc.getCode(), sc.getName());
+        return String.format("%s: %s - %s", escape(sct.getName()), escape(sc.getCode()), escape(sc.getName()));
       })
       .collect(Collectors.joining("|"));
   }
