c6be96a580adb40184a7442db2968a0c67ef1a6d
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2025-04-07 10:21:35 +0300

    MODBULKOPS-190 - Adding Subject Column for Instance Records in Preview and Are you sure? Forms (#400)
    * MODBULKOPS-190 Added subject

---
+++ src/main/java/org/folio/bulkops/service/SubjectReferenceService.java
@@ -0,0 +1,71 @@
+package org.folio.bulkops.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.bulkops.client.SubjectSourcesClient;
+import org.folio.bulkops.client.SubjectTypesClient;
+import org.folio.bulkops.exception.NotFoundException;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.FolioModuleMetadata;
+import org.folio.spring.scope.FolioExecutionContextSetter;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import static java.util.Objects.isNull;
+import static org.folio.bulkops.util.Constants.QUERY_PATTERN_NAME;
+import static org.folio.bulkops.util.FolioExecutionContextUtil.prepareContextForTenant;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class SubjectReferenceService {
+
+  private final FolioExecutionContext folioExecutionContext;
+  private final FolioModuleMetadata folioModuleMetadata;
+  private final SubjectSourcesClient subjectSourcesClient;
+  private final SubjectTypesClient subjectTypesClient;
+
+  @Cacheable(cacheNames = "subjectSourceNames")
+  public String getSubjectSourceNameById(String id, String tenantId) {
+    log.info("getSubjectSourceNameById: {}, {}", id, tenantId);
+    if (isNull(tenantId)) {
+      tenantId = folioExecutionContext.getTenantId();
+    }
+    try (var ignored = new FolioExecutionContextSetter(prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
+      return subjectSourcesClient.getById(id).getName();
+    } catch (NotFoundException e) {
+      log.error("Subject source was not found by id={}", id);
+      return id;
+    }
+  }
+
+  @Cacheable(cacheNames = "subjectTypeNames")
+  public String getSubjectTypeNameById(String id, String tenantId) {
+    log.info("getSubjectTypeNameById: {}, {}", id, tenantId);
+    if (isNull(tenantId)) {
+      tenantId = folioExecutionContext.getTenantId();
+    }
+    try (var ignored = new FolioExecutionContextSetter(prepareContextForTenant(tenantId, folioModuleMetadata, folioExecutionContext))) {
+      return subjectTypesClient.getById(id).getName();
+    } catch (NotFoundException e) {
+      log.error("Subject type was not found by id={}", id);
+      return id;
+    }
+  }
+
+  @Cacheable(cacheNames = "subjectSourceIds")
+  public String getSubjectSourceIdByName(String name) {
+    var subjectSources = subjectSourcesClient.getByQuery(String.format(QUERY_PATTERN_NAME, name));
+    return subjectSources.getSubjectSources().isEmpty() ?
+      name :
+      subjectSources.getSubjectSources().get(0).getId();
+  }
+
+  @Cacheable(cacheNames = "subjectTypeIds")
+  public String getSubjectTypeIdByName(String name) {
+    var subjectTypes = subjectTypesClient.getByQuery(String.format(QUERY_PATTERN_NAME, name));
+    return subjectTypes.getSubjectTypes().isEmpty() ?
+      name :
+      subjectTypes.getSubjectTypes().get(0).getId();
+  }
+}
--- src/test/java/org/folio/bulkops/service/MarcToUnifiedTableRowMapperTest.java
+++ src/test/java/org/folio/bulkops/service/MarcToUnifiedTableRowMapperTest.java
@@ -2,6 +2,7 @@ package org.folio.bulkops.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.folio.bulkops.domain.bean.Instance.INSTANCE_ELECTRONIC_ACCESS;
+import static org.folio.bulkops.domain.bean.Instance.INSTANCE_SUBJECT;
 import static org.folio.bulkops.util.Constants.DATE_TIME_CONTROL_FIELD;
 import static org.folio.bulkops.util.Constants.NON_PRINTING_DELIMITER;
 
@@ -80,5 +81,121 @@ class MarcToUnifiedTableRowMapperTest extends BaseTest {
 
     assertThat(rowData.getFirst()).isEqualTo("Related resource%s;-%s;text%s;materials%s;public note"
       .formatted(NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER));
+  }
+
+  @Test
+  void processRecordWithSubjectIfSubfieldIsEmptyTest() {
+    var marcRecord = new RecordImpl();
+    marcRecord.setLeader(new LeaderImpl("04295nam a22004573a 4500"));
+
+    var controlField = new ControlFieldImpl(DATE_TIME_CONTROL_FIELD, "20240101100202.4");
+    marcRecord.addVariableField(controlField);
+
+    var dataField = new DataFieldImpl("600", '1', '2');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+    var rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_SUBJECT), true);
+
+    assertThat(rowData.getFirst()).isEqualTo("text subject c subject d%s;Medical Subject Headings%s;Personal name"
+      .formatted(NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER));
+  }
+
+  @Test
+  void processRecordWithSubjectAndAllTypesTest() {
+    var marcRecord = new RecordImpl();
+    marcRecord.setLeader(new LeaderImpl("04295nam a22004573a 4500"));
+
+    var controlField = new ControlFieldImpl(DATE_TIME_CONTROL_FIELD, "20240101100202.4");
+    marcRecord.addVariableField(controlField);
+
+    var dataField = new DataFieldImpl("600", '1', '2');
+    dataField.addSubfield(new SubfieldImpl('a', "a 600"));
+    dataField.addSubfield(new SubfieldImpl('b', "b text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    dataField.addSubfield(new SubfieldImpl('d', ""));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("610", '1', '3');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c 2"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("611", '3', '2');
+    dataField.addSubfield(new SubfieldImpl('a', "611 a"));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("630", '1', '5');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("647", '1', '2');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("648", '2', '7');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('2', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("650", '1', '9');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("651", ' ', '2');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("651", '0', '1');
+    dataField.addSubfield(new SubfieldImpl('a', "a"));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    dataField = new DataFieldImpl("655", '1', ' ');
+    dataField.addSubfield(new SubfieldImpl('a', ""));
+    dataField.addSubfield(new SubfieldImpl('b', "text"));
+    dataField.addSubfield(new SubfieldImpl('c', "subject c"));
+    dataField.addSubfield(new SubfieldImpl('d', "subject d"));
+    marcRecord.addVariableField(dataField);
+
+    var rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_SUBJECT), true);
+
+    var expectedRowData = "a 600 b text subject c subject d\u001f;Medical Subject Headings\u001f;Personal name\u001f|" +
+      "text subject c subject c 2 subject d\u001f;National Agriculture Library subject authority file\u001f;Corporate name\u001f|" +
+      "611 a text subject c subject d\u001f;Medical Subject Headings\u001f;Meeting name\u001f|" +
+      "text subject c subject d\u001f;Canadian Subject Headings\u001f;Uniform title\u001f|" +
+      "text subject c subject d\u001f;Medical Subject Headings\u001f;Named event\u001f|" +
+      "text subject c subject d\u001f;text\u001f;Chronological term\u001f|" +
+      "text subject c subject d\u001f;-\u001f;Topical term\u001f|" +
+      "text subject c subject d\u001f;Medical Subject Headings\u001f;Geographic name\u001f|" +
+      "a text subject c subject d\u001f;Library of Congress Children’s and Young Adults' Subject Headings\u001f;Geographic name\u001f|" +
+      "text subject c subject d\u001f;-\u001f;Genre/Form";
+
+    assertThat(rowData.getFirst()).isEqualTo(expectedRowData);
   }
 }
