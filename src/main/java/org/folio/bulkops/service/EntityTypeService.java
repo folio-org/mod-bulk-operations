9dbb540f90a9a7cb15577bcba3b422eb2dbcc989
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-05-26 21:22:05 +0300

    MODBULKOPS-520 Added hardcoded ids

--- src/main/java/org/folio/bulkops/service/EntityTypeService.java
+++ src/main/java/org/folio/bulkops/service/EntityTypeService.java
@@ -1,11 +1,16 @@
 package org.folio.bulkops.service;
 
+import static org.folio.bulkops.util.Constants.HOLDINGS_ENTITY_TYPE_ID;
+import static org.folio.bulkops.util.Constants.INSTANCE_ENTITY_TYPE_ID;
+import static org.folio.bulkops.util.Constants.ITEM_ENTITY_TYPE_ID;
+import static org.folio.bulkops.util.Constants.USER_ENTITY_TYPE_ID;
+
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import org.folio.bulkops.client.EntityTypeClient;
 import org.folio.bulkops.domain.dto.EntityType;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,13 +18,24 @@ public class EntityTypeService {
   private final EntityTypeClient entityTypeClient;
 
   public EntityType getEntityTypeById(UUID entityTypeId) {
+    var id = entityTypeClient.getEntityType(entityTypeId).getId();
     var alias = entityTypeClient.getEntityType(entityTypeId).getLabelAlias();
     return switch (alias) {
       case "Items" -> EntityType.ITEM;
       case "Users" -> EntityType.USER;
       case "Holdings" -> EntityType.HOLDINGS_RECORD;
       case "Instances" -> EntityType.INSTANCE;
-      default -> throw new IllegalArgumentException(String.format("Entity type %s is not supported", alias));
+      default -> getAliasOrId(id, alias);
+    };
+  }
+
+  private EntityType getAliasOrId(String id, String alias) {
+    return switch (id) {
+      case ITEM_ENTITY_TYPE_ID -> EntityType.ITEM;
+      case USER_ENTITY_TYPE_ID -> EntityType.USER;
+      case HOLDINGS_ENTITY_TYPE_ID -> EntityType.HOLDINGS_RECORD;
+      case INSTANCE_ENTITY_TYPE_ID -> EntityType.INSTANCE;
+      default -> throw new IllegalArgumentException(String.format("Entity type id=%s and alias=%s is not supported", id, alias));
     };
   }
 }
--- src/test/java/org/folio/bulkops/service/EntityTypeServiceTest.java
+++ src/test/java/org/folio/bulkops/service/EntityTypeServiceTest.java
@@ -3,6 +3,8 @@ package org.folio.bulkops.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.UUID;
+
 import org.folio.bulkops.client.EntityTypeClient;
 import org.folio.querytool.domain.dto.EntityType;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +13,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 class EntityTypeServiceTest {
@@ -23,11 +23,18 @@ class EntityTypeServiceTest {
 
   @ParameterizedTest
   @CsvSource(textBlock = """
-    ae6a4972-0e0d-4069-9936-99ba6e91658d  | Users | USER
-    a5f253dc-3215-46bb-9714-2b26d8b3b613  | Items | ITEM
+    ae6a4972-0e0d-4069-9936-99ba6e91658d | Users | USER
+    a5f253dc-3215-46bb-9714-2b26d8b3b613 | Items | ITEM
+    65f253dc-3215-46bb-9714-2b26d8b3b613 | Holdings | HOLDINGS_RECORD
+    75f253dc-3215-46bb-9714-2b26d8b3b613 | Instances | INSTANCE
+    d0213d22-32cf-490f-9196-d81c3c66e53f | ItemFr | ITEM
+    ddc93926-d15a-4a45-9d9c-93eadc3d9bbf | UserFr | USER
+    8418e512-feac-4a6a-a56d-9006aab31e33 | HoldFr | HOLDINGS_RECORD
+    6b08439b-4f8e-4468-8046-ea620f5cfb74 | InstFr | INSTANCE
     """, delimiter = '|')
   void testGetEntityType(UUID entityTypeId, String alias, org.folio.bulkops.domain.dto.EntityType expectedType) {
-    when(entityTypeClient.getEntityType(entityTypeId)).thenReturn(new EntityType().labelAlias(alias));
+    when(entityTypeClient.getEntityType(entityTypeId)).thenReturn(new EntityType().labelAlias(alias)
+            .id(entityTypeId.toString()));
 
     var actualType = entityTypeService.getEntityTypeById(entityTypeId);
 
