003c5f36f7e84c26e9f4238539edc420f5321271
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2025-05-27 11:51:49 +0300

    MODBULKOPS-520 - Error after "Run Query" when the UI is set to a language other than English (#419)
    * MODBULKOPS-520 Added hardcoded ids

--- src/test/java/org/folio/bulkops/service/EntityTypeServiceTest.java
+++ src/test/java/org/folio/bulkops/service/EntityTypeServiceTest.java
@@ -3,6 +3,8 @@ package org.folio.bulkops.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.UUID;
+
 import org.folio.bulkops.client.EntityTypeClient;
 import org.folio.querytool.domain.dto.EntityType;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +13,6 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 class EntityTypeServiceTest {
@@ -23,11 +23,17 @@ class EntityTypeServiceTest {
 
   @ParameterizedTest
   @CsvSource(textBlock = """
-    ae6a4972-0e0d-4069-9936-99ba6e91658d  | Users | USER
-    a5f253dc-3215-46bb-9714-2b26d8b3b613  | Items | ITEM
+    7663cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_user_details | Users | USER
+    3463cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_item_details | Items | ITEM
+    2363cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_holdings_record | Holdings | HOLDINGS_RECORD
+    1163cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_instances | Instances | INSTANCE
+    5363cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_item_details | ItemFr | ITEM
+    7863cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_user_details | UserFr | USER
+    8963cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_holdings_record | HoldFr | HOLDINGS_RECORD
+    9063cc37-ed7c-4905-9ef1-a23ac4d4f9df | composite_instances | InstFr | INSTANCE
     """, delimiter = '|')
-  void testGetEntityType(UUID entityTypeId, String alias, org.folio.bulkops.domain.dto.EntityType expectedType) {
-    when(entityTypeClient.getEntityType(entityTypeId)).thenReturn(new EntityType().labelAlias(alias));
+  void testGetEntityType(UUID entityTypeId, String name, String alias, org.folio.bulkops.domain.dto.EntityType expectedType) {
+    when(entityTypeClient.getEntityType(entityTypeId)).thenReturn(new EntityType().name(name).labelAlias(alias));
 
     var actualType = entityTypeService.getEntityTypeById(entityTypeId);
 
