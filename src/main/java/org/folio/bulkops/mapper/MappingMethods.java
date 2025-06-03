bb54f0879616c2d2fde3878eb4e2161660a4fbfe
Kriti Jain <kjain@ebsco.com>
2025-06-03 14:07:10 -0500

    re

--- src/main/java/org/folio/bulkops/mapper/MappingMethods.java
+++ src/main/java/org/folio/bulkops/mapper/MappingMethods.java
@@ -1,36 +1,37 @@
 package org.folio.bulkops.mapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.folio.bulkops.domain.dto.BulkOperationRuleCollection;
-import org.folio.bulkops.domain.dto.BulkOperationMarcRuleCollection;
+import org.folio.bulkops.domain.dto.BulkOperationRule;
+import org.folio.bulkops.domain.dto.BulkOperationMarcRule;
 import org.springframework.stereotype.Component;
 import org.mapstruct.Named;
 
 import java.time.OffsetDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class MappingMethods {
 
-//  private final ObjectMapper objectMapper;
-//
-//  public MappingMethods(ObjectMapper objectMapper) {
-//    this.objectMapper = objectMapper;
- // }
+  private final ObjectMapper objectMapper;
+
+  public MappingMethods(ObjectMapper objectMapper) {
+    this.objectMapper = objectMapper;
+  }
   public Date offsetDateTimeAsDate(OffsetDateTime offsetDateTime) {
     return offsetDateTime == null ? null : Date.from(offsetDateTime.toInstant());
   }
-//
-//  @Named("mapToBulkOperationRuleCollection")
-//  public BulkOperationRuleCollection mapToBulkOperationRuleCollection(Map<String, Object> source) {
+
+//  @Named("mapToBulkOperationRule")
+//  public BulkOperationRule mapToBulkOperationRuleCollection(List<BulkOperationRule> source) {
 //    if (source == null) return null;
-//    return objectMapper.convertValue(source, BulkOperationRuleCollection.class);
+//    return objectMapper.convertValue(source, BulkOperationRule.class);
 //  }
 //
-//  @Named("mapToBulkOperationMarcRuleCollection")
-//  public BulkOperationMarcRuleCollection mapToBulkOperationMarcRuleCollection(Map<String, Object> source) {
+//  @Named("mapToBulkOperationMarcRule")
+//  public BulkOperationMarcRule mapToBulkOperationMarcRuleCollection(List<BulkOperationMarcRule> source) {
 //    if (source == null) return null;
-//    return objectMapper.convertValue(source, BulkOperationMarcRuleCollection.class);
+//    return objectMapper.convertValue(source, BulkOperationMarcRule.class);
 //  }
 }
