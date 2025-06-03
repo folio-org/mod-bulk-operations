406b4a5e39a309e97de6404d3effa557e25e1dc0
Kriti Jain <kjain@ebsco.com>
2025-06-03 15:10:53 -0500

    re

--- src/main/java/org/folio/bulkops/mapper/MappingMethods.java
+++ src/main/java/org/folio/bulkops/mapper/MappingMethods.java
@@ -1,5 +1,6 @@
 package org.folio.bulkops.mapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.folio.bulkops.domain.dto.BulkOperationRule;
 import org.folio.bulkops.domain.dto.BulkOperationMarcRule;
@@ -14,14 +15,33 @@ import java.util.Map;
 @Component
 public class MappingMethods {
 
-  private final ObjectMapper objectMapper;
-
-  public MappingMethods(ObjectMapper objectMapper) {
-    this.objectMapper = objectMapper;
-  }
+//  private final ObjectMapper objectMapper;
+//
+//  public MappingMethods(ObjectMapper objectMapper) {
+//    this.objectMapper = objectMapper;
+//  }
   public Date offsetDateTimeAsDate(OffsetDateTime offsetDateTime) {
     return offsetDateTime == null ? null : Date.from(offsetDateTime.toInstant());
   }
+
+//  @Named("mapMarcRuleJsonToObject")
+//  public org.folio.bulkops.domain.dto.BulkOperationMarcRuleCollection mapMarcRuleJsonToObject(String json) {
+//    try {
+//      return objectMapper.readValue(json, org.folio.bulkops.domain.dto.BulkOperationMarcRuleCollection.class);
+//    } catch (JsonProcessingException e) {
+//      throw new RuntimeException("Invalid MARC rule JSON", e);
+//    }
+//  }
+//
+//  @Named("mapRuleJsonToObject")
+//  public org.folio.bulkops.domain.dto.BulkOperationRuleCollection mapRuleJsonToObject(String json) {
+//    try {
+//      return objectMapper.readValue(json, org.folio.bulkops.domain.dto.BulkOperationRuleCollection.class);
+//    } catch (JsonProcessingException e) {
+//      throw new RuntimeException("Invalid MARC rule JSON", e);
+//    }
+//  }
+
 
 //  @Named("mapToBulkOperationRule")
 //  public BulkOperationRule mapToBulkOperationRuleCollection(List<BulkOperationRule> source) {
