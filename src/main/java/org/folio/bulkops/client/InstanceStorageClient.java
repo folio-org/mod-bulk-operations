ba07f023994d8d4346d4aa406fb29ef78c682614
siarhei-charniak <siarhei_charniak@epam.com>
2025-06-25 15:05:30 +0300

    MODBULKOPS-542 - Columns not populated for holdings and items record when triggering bulk edit by query

--- src/main/java/org/folio/bulkops/client/InstanceStorageClient.java
+++ src/main/java/org/folio/bulkops/client/InstanceStorageClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-@FeignClient(name = "inventory-storage/instances")
+@FeignClient(name = "instance-storage/instances")
 public interface InstanceStorageClient {
   @PutMapping(value = "/{instanceId}")
   void updateInstance(@RequestBody Instance instance, @PathVariable String instanceId);
