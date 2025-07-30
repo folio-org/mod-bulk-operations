be4a3c7dc23bb6c8d8a5f072097163a8858e8cc2
siarhei-charniak <siarhei_charniak@epam.com>
2025-07-30 16:04:48 +0300

    MODBULKOPS-516 - FAILED (UNKNOWN) in Electronic access, Subject, Classification columns in .csv file with updated records (#457)

--- src/main/java/org/folio/bulkops/util/FolioExecutionContextUtil.java
+++ src/main/java/org/folio/bulkops/util/FolioExecutionContextUtil.java
@@ -1,11 +1,9 @@
 package org.folio.bulkops.util;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.collections4.MapUtils;
-import org.apache.commons.lang3.SerializationUtils;
 import org.folio.spring.DefaultFolioExecutionContext;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
@@ -20,8 +18,7 @@ public class FolioExecutionContextUtil {
 
   public static FolioExecutionContext prepareContextForTenant(String tenantId, FolioModuleMetadata folioModuleMetadata, FolioExecutionContext context) {
     if (MapUtils.isNotEmpty(context.getOkapiHeaders())) {
-      // create deep copy of headers in order to make switching context thread safe
-      var headersCopy = SerializationUtils.clone((HashMap<String, Collection<String>>) context.getAllHeaders());
+      var headersCopy = new HashMap<>(context.getAllHeaders());
       headersCopy.put(XOkapiHeaders.TENANT, List.of(tenantId));
       log.info("FOLIO context initialized with tenant {}", tenantId);
       return new DefaultFolioExecutionContext(folioModuleMetadata, headersCopy);
