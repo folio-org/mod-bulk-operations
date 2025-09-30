c8501926544e86f84f2b12c6c27e20698db3b4a0
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-09-30 12:35:19 +0300

    MODBULKOPS-560 Fixed coverage

--- src/test/java/org/folio/bulkops/util/MarcCsvHelperTest.java
+++ src/test/java/org/folio/bulkops/util/MarcCsvHelperTest.java
@@ -2,21 +2,15 @@ package org.folio.bulkops.util;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.folio.bulkops.domain.bean.Instance.INSTANCE_HRID;
 import static org.folio.bulkops.domain.dto.OperationStatusType.APPLY_CHANGES;
 import static org.folio.bulkops.domain.dto.OperationStatusType.REVIEW_CHANGES;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import lombok.SneakyThrows;
 import org.folio.bulkops.BaseTest;
 import org.folio.bulkops.client.MappingRulesClient;
 import org.folio.bulkops.client.RemoteFileSystemClient;
-import org.folio.bulkops.domain.bean.Instance;
 import org.folio.bulkops.domain.dto.BulkOperationMarcRule;
 import org.folio.bulkops.domain.dto.BulkOperationMarcRuleCollection;
 import org.folio.bulkops.domain.dto.EntityType;
@@ -24,9 +18,6 @@ import org.folio.bulkops.domain.dto.InstanceNoteType;
 import org.folio.bulkops.domain.dto.OperationStatusType;
 import org.folio.bulkops.domain.entity.BulkOperation;
 import org.folio.bulkops.service.InstanceReferenceService;
-import org.folio.bulkops.service.Marc21ReferenceProvider;
-import org.folio.bulkops.service.MarcToUnifiedTableRowMapper;
-import org.folio.bulkops.service.NoteTableUpdater;
 import org.folio.bulkops.service.RuleService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -38,12 +29,9 @@ import org.marc4j.marc.impl.SubfieldImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
 class MarcCsvHelperTest extends BaseTest {
@@ -211,110 +199,30 @@ class MarcCsvHelperTest extends BaseTest {
   }
 
   @Test
-  void getChangedMarcData_shouldPutEmptyStringValue() throws Exception {
-    Marc21ReferenceProvider marc21ReferenceProvider = mock(Marc21ReferenceProvider.class);
-    var marcCsvHelper = new MarcCsvHelper(
-            mock(NoteTableUpdater.class),
-            mock(MarcToUnifiedTableRowMapper.class),
-            remoteFileSystemClient,
-            ruleService,
-            marc21ReferenceProvider,
-            objectMapper
-    );
-
-    var instanceHeaderNames = UnifiedTableHeaderBuilder.getEmptyTableWithHeaders(Instance.class)
-            .getHeader().stream().map(org.folio.bulkops.domain.dto.Cell::getValue).toList();
-    var hridIndex = instanceHeaderNames.indexOf(INSTANCE_HRID);
-    var option = instanceHeaderNames.get(hridIndex + 1); // pick a column after hrid
-
-    var fileName = "test.csv";
-    var bulkOperation = BulkOperation.builder()
-            .id(UUID.randomUUID())
-            .status(OperationStatusType.REVIEW_CHANGES)
+  @SneakyThrows
+  void shouldEnrichCsvWithEmptyValue() {
+    var fileName = "file.csv";
+    var operationId = UUID.randomUUID();
+    var operation = BulkOperation.builder()
+            .id(operationId)
+            .status(REVIEW_CHANGES)
             .entityType(EntityType.INSTANCE_MARC)
             .linkToModifiedRecordsMarcCsvFile(fileName)
             .build();
 
-    // Prepare CSV: hrid, option, ...; row: hridValue, "", ...
-    String[] header = instanceHeaderNames.toArray(new String[0]);
-    String[] row = new String[header.length];
-    row[hridIndex] = "hridValue";
-    row[instanceHeaderNames.indexOf(option)] = ""; // empty string
+    var content = new FileInputStream("src/test/resources/files/instance.csv").readAllBytes();
 
-    var csvContent = String.join(",", header) + "\n" + String.join(",", row) + "\n";
-
-    // Mock ObjectReader and all relevant ObjectMapper methods
-    ObjectReader objectReader = mock(ObjectReader.class);
-    when(objectReader.forType(any(Class.class))).thenReturn(objectReader);
-    when(remoteFileSystemClient.get(fileName)).thenReturn(new ByteArrayInputStream(csvContent.getBytes()));
-    when(ruleService.getMarcRules(bulkOperation.getId()))
+    when(mappingRulesClient.getMarcBibMappingRules())
+            .thenReturn(Files.readString(Path.of("src/test/resources/files/mappingRulesResponse.json")));
+    when(remoteFileSystemClient.get(fileName))
+            .thenReturn(new FileInputStream("src/test/resources/files/marc_csv_empty_notes.csv"));
+    when(ruleService.getMarcRules(operationId))
             .thenReturn(new BulkOperationMarcRuleCollection()
-                    .bulkOperationMarcRules(singletonList(new BulkOperationMarcRule().tag(option))));
-    when(marc21ReferenceProvider.getChangedOptionsSetForCsv(any())).thenReturn(Set.of(option));
-    doNothing().when(marc21ReferenceProvider).updateMappingRules();
+                    .bulkOperationMarcRules(singletonList(new BulkOperationMarcRule().tag("500"))));
 
-    // Use reflection to call private method
-    var method = MarcCsvHelper.class.getDeclaredMethod("getChangedMarcData", BulkOperation.class);
-    method.setAccessible(true);
-    var result = method.invoke(marcCsvHelper, bulkOperation);
+    var res = marcCsvHelper.enrichCsvWithMarcChanges(content, operation);
 
-    assertThat(result).isInstanceOf(Map.class);
-    var resultMap = (Map<String, Map<String, String>>) result;
-    assertThat(resultMap).containsKey("hridValue");
-    assertThat(resultMap.get("hridValue")).containsEntry(option, "");
-  }
-
-  @Test
-  void getChangedMarcData_shouldSkipWhenValueIsNull() throws Exception {
-    Marc21ReferenceProvider marc21ReferenceProvider = mock(Marc21ReferenceProvider.class);
-    var marcCsvHelper = new MarcCsvHelper(
-            mock(NoteTableUpdater.class),
-            mock(MarcToUnifiedTableRowMapper.class),
-            remoteFileSystemClient,
-            ruleService,
-            marc21ReferenceProvider,
-            objectMapper
-    );
-
-    var instanceHeaderNames = UnifiedTableHeaderBuilder.getEmptyTableWithHeaders(Instance.class)
-            .getHeader().stream().map(org.folio.bulkops.domain.dto.Cell::getValue).toList();
-    var hridIndex = instanceHeaderNames.indexOf(INSTANCE_HRID);
-    var option = instanceHeaderNames.get(hridIndex + 1); // pick a column after hrid
-
-    var fileName = "test_null_value.csv";
-    var bulkOperation = BulkOperation.builder()
-            .id(UUID.randomUUID())
-            .status(OperationStatusType.REVIEW_CHANGES)
-            .entityType(EntityType.INSTANCE_MARC)
-            .linkToModifiedRecordsMarcCsvFile(fileName)
-            .build();
-
-    // Prepare CSV: hrid, option, ...; row: hridValue, null, ...
-    String[] header = instanceHeaderNames.toArray(new String[0]);
-    String[] row = new String[header.length];
-    row[hridIndex] = "hridValue";
-    // Explicitly set the option column to null
-    row[instanceHeaderNames.indexOf(option)] = null;
-
-    var csvContent = String.join(",", header) + "\n" + String.join(",", row) + "\n";
-
-    ObjectReader objectReader = mock(ObjectReader.class);
-    when(objectReader.forType(any(Class.class))).thenReturn(objectReader);
-    when(remoteFileSystemClient.get(fileName)).thenReturn(new ByteArrayInputStream(csvContent.getBytes()));
-    when(ruleService.getMarcRules(bulkOperation.getId()))
-            .thenReturn(new BulkOperationMarcRuleCollection()
-                    .bulkOperationMarcRules(singletonList(new BulkOperationMarcRule().tag(option))));
-    when(marc21ReferenceProvider.getChangedOptionsSetForCsv(any())).thenReturn(Set.of(option));
-    doNothing().when(marc21ReferenceProvider).updateMappingRules();
-
-    var method = MarcCsvHelper.class.getDeclaredMethod("getChangedMarcData", BulkOperation.class);
-    method.setAccessible(true);
-    var result = method.invoke(marcCsvHelper, bulkOperation);
-
-    assertThat(result).isInstanceOf(Map.class);
-    var resultMap = (Map<String, Map<String, String>>) result;
-    assertThat(resultMap).containsKey("hridValue");
-    // Should not contain the option key since line[index] == null
-    assertThat(resultMap.get("hridValue").get("Source")).isEqualTo("null");
+    var expectedInstanceNotes = "";
+    assertThat(new String(res)).contains(expectedInstanceNotes);
   }
 }
