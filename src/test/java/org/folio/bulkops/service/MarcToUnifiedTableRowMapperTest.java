780b1af0c8168912efff3bf70ab1c2b72647140d
Mikita Siadykh <mikita_siadykh@epam.com>
2025-10-09 11:05:57 +0300

    MODBULKOPS-554 Update 'Materials specified' sub-column name in .csv files (#496)

--- src/test/java/org/folio/bulkops/service/MarcToUnifiedTableRowMapperTest.java
+++ src/test/java/org/folio/bulkops/service/MarcToUnifiedTableRowMapperTest.java
@@ -40,13 +40,13 @@ class MarcToUnifiedTableRowMapperTest extends BaseTest {
     var dataField = new DataFieldImpl("856", '4', '2');
     dataField.addSubfield(new SubfieldImpl('u', "url"));
     dataField.addSubfield(new SubfieldImpl('y', "text"));
-    dataField.addSubfield(new SubfieldImpl('3', "materials"));
+    dataField.addSubfield(new SubfieldImpl('3', "material"));
     dataField.addSubfield(new SubfieldImpl('z', "public note"));
     marcRecord.addVariableField(dataField);
     var rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_ELECTRONIC_ACCESS), true);
 
-    assertThat(rowData.getFirst()).isEqualTo("URL relationship;URI;Link text;Materials specified;URL public note\n" +
-      "Related resource;url;text;materials;public note");
+    assertThat(rowData.getFirst()).isEqualTo("URL relationship;URI;Link text;Material specified;URL public note\n" +
+      "Related resource;url;text;material;public note");
   }
 
   @Test
@@ -61,18 +61,18 @@ class MarcToUnifiedTableRowMapperTest extends BaseTest {
     dataField.addSubfield(new SubfieldImpl('u', "url"));
     dataField.addSubfield(new SubfieldImpl('y', "text"));
     dataField.addSubfield(new SubfieldImpl('y', "text2"));
-    dataField.addSubfield(new SubfieldImpl('3', "materials"));
+    dataField.addSubfield(new SubfieldImpl('3', "material"));
     dataField.addSubfield(new SubfieldImpl('z', "public note"));
     dataField.addSubfield(new SubfieldImpl('z', "public note2"));
     marcRecord.addVariableField(dataField);
     var rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_ELECTRONIC_ACCESS), true);
 
-    assertThat(rowData.getFirst()).isEqualTo("URL relationship;URI;Link text;Materials specified;URL public note\n" +
-      "Related resource;url;text text2;materials;public note public note2");
+    assertThat(rowData.getFirst()).isEqualTo("URL relationship;URI;Link text;Material specified;URL public note\n" +
+      "Related resource;url;text text2;material;public note public note2");
 
     rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_ELECTRONIC_ACCESS), false);
 
-    assertThat(rowData.getFirst()).isEqualTo("Related resource%s;url%s;text text2%s;materials%s;public note public note2"
+    assertThat(rowData.getFirst()).isEqualTo("Related resource%s;url%s;text text2%s;material%s;public note public note2"
       .formatted(NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER, NON_PRINTING_DELIMITER));
   }
 
@@ -87,17 +87,17 @@ class MarcToUnifiedTableRowMapperTest extends BaseTest {
     var dataField = new DataFieldImpl("856", '4', '2');
     dataField.addSubfield(new SubfieldImpl('u', ""));
     dataField.addSubfield(new SubfieldImpl('y', "text"));
-    dataField.addSubfield(new SubfieldImpl('3', "materials"));
+    dataField.addSubfield(new SubfieldImpl('3', "material"));
     dataField.addSubfield(new SubfieldImpl('z', "public note"));
     marcRecord.addVariableField(dataField);
     var rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_ELECTRONIC_ACCESS), true);
 
-    assertThat(rowData.getFirst()).isEqualTo(("URL relationship;URI;Link text;Materials specified;URL public note\n" +
-      "Related resource;-;text;materials;public note"));
+    assertThat(rowData.getFirst()).isEqualTo(("URL relationship;URI;Link text;Material specified;URL public note\n" +
+      "Related resource;-;text;material;public note"));
 
     rowData = marcToUnifiedTableRowMapper.processRecord(marcRecord, List.of(INSTANCE_ELECTRONIC_ACCESS), false);
 
-    assertThat(rowData.getFirst()).isEqualTo(("Related resource\u001f;-\u001f;text\u001f;materials\u001f;public note"));
+    assertThat(rowData.getFirst()).isEqualTo(("Related resource\u001f;-\u001f;text\u001f;material\u001f;public note"));
   }
 
   @Test
