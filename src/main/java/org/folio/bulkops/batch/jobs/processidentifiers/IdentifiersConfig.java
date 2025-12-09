13c415e4293ce5bd38ad62a1b937b6a596115d7e
Siarhei Charniak <siarhei_charniak@epam.com>
2025-12-09 13:42:53 +0300

    MODBULKOPS-590

--- src/main/java/org/folio/bulkops/batch/jobs/processidentifiers/IdentifiersConfig.java
+++ src/main/java/org/folio/bulkops/batch/jobs/processidentifiers/IdentifiersConfig.java
@@ -2,9 +2,11 @@ package org.folio.bulkops.batch.jobs.processidentifiers;
 
 import static org.folio.bulkops.util.Constants.IDENTIFIERS_FILE_NAME;
 
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.folio.bulkops.client.RemoteFileSystemClient;
 import org.folio.bulkops.domain.bean.ItemIdentifier;
+import org.folio.bulkops.exception.BulkEditException;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
@@ -15,7 +17,7 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.ByteArrayResource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,17 +29,22 @@ public class IdentifiersConfig {
   public FlatFileItemReader<ItemIdentifier> csvItemIdentifierReader(
       @Value("#{jobParameters['" + IDENTIFIERS_FILE_NAME + "']}") String uploadedFileName,
       @Value("#{stepExecutionContext['offset']}") Long offset,
-      @Value("#{stepExecutionContext['limit']}") Long limit) {
-    var builder =
-        new FlatFileItemReaderBuilder<ItemIdentifier>()
-            .name("csvItemIdentifierReader")
-            .resource(new InputStreamResource(remoteFileSystemClient.get(uploadedFileName)))
-            .linesToSkip(Math.toIntExact(offset))
-            .lineMapper(lineMapper());
-    if (limit != null && limit > 0) {
-      builder.maxItemCount(Math.toIntExact(limit));
+      @Value("#{stepExecutionContext['limit']}") Long limit)
+      throws BulkEditException {
+    try (var is = remoteFileSystemClient.get(uploadedFileName)) {
+      var builder =
+          new FlatFileItemReaderBuilder<ItemIdentifier>()
+              .name("csvItemIdentifierReader")
+              .resource(new ByteArrayResource(is.readAllBytes()))
+              .linesToSkip(Math.toIntExact(offset))
+              .lineMapper(lineMapper());
+      if (limit != null && limit > 0) {
+        builder.maxItemCount(Math.toIntExact(limit));
+      }
+      return builder.build();
+    } catch (IOException e) {
+      throw new BulkEditException("Failed to read file " + IDENTIFIERS_FILE_NAME);
     }
-    return builder.build();
   }
 
   @Bean
