96eae4dd22d7da629cd103d95530a22bf818bd3f
obozhko-folio <Oleksandr_Bozhko@epam.com>
2026-02-25 13:05:45 +0200

    MODBULKOPS-581 Fixed request header

--- src/main/java/org/folio/bulkops/client/EntityTypeClient.java
+++ src/main/java/org/folio/bulkops/client/EntityTypeClient.java
@@ -4,14 +4,14 @@ import java.util.UUID;
 import org.folio.bulkops.domain.bean.EntityTypeSummaries;
 import org.folio.querytool.domain.dto.EntityType;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
 @HttpExchange(url = "entity-types")
 public interface EntityTypeClient {
   @GetExchange("/{entityTypeId}")
-  EntityType getEntityType(@RequestHeader UUID entityTypeId);
+  EntityType getEntityType(@PathVariable UUID entityTypeId);
 
   @GetExchange(accept = MediaType.APPLICATION_JSON_VALUE)
   EntityTypeSummaries getEntityTypeSummaries();
