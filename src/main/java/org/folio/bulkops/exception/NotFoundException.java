bbc415cb83cb7a2da1494d6e179f6c38af9c5948
obozhko-folio <Oleksandr_Bozhko@epam.com>
2026-03-05 18:28:18 +0200

    MODBULKOPS-581 Fixed HttpServiceProxyFactory

--- src/main/java/org/folio/bulkops/exception/NotFoundException.java
+++ src/main/java/org/folio/bulkops/exception/NotFoundException.java
@@ -4,8 +4,4 @@ public class NotFoundException extends RuntimeException {
   public NotFoundException(String message) {
     super(message);
   }
-
-  public NotFoundException() {
-    super();
-  }
 }
