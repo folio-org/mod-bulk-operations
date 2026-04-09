b332237bc1ca85e78714bfb65720fc1da16e454b
obozhko-folio <Oleksandr_Bozhko@epam.com>
2026-04-09 14:16:42 +0300

    MODBULKOPS-622 Fix local app

--- src/main/java/org/folio/bulkops/domain/converter/ExpirationDateConverter.java
+++ src/main/java/org/folio/bulkops/domain/converter/ExpirationDateConverter.java
@@ -1,5 +1,9 @@
 package org.folio.bulkops.domain.converter;
 
+import static org.folio.bulkops.util.Constants.DATE_WITHOUT_TIME_FORMATTER;
+import static org.folio.bulkops.util.Constants.UTC_ZONE;
+
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.TimeZone;
 import lombok.extern.log4j.Log4j2;
@@ -7,6 +11,19 @@ import org.folio.bulkops.service.UserReferenceHelper;
 
 @Log4j2
 public class ExpirationDateConverter extends DateWithoutTimeConverter {
+
+  @Override
+  public Date convertToObject(String value) {
+    var zoneId = UTC_ZONE;
+    var locale = UserReferenceHelper.service().getTenantLocale();
+    if (locale != null && locale.getTimezone() != null) {
+      zoneId = TimeZone.getTimeZone(locale.getTimezone()).toZoneId();
+    } else {
+      log.error("Locale or timezone is null, using default timezone for parsing expiration date");
+    }
+    return Date.from(
+        LocalDate.parse(value, DATE_WITHOUT_TIME_FORMATTER).atStartOfDay(zoneId).toInstant());
+  }
 
   @Override
   public String convertToString(Date object) {
