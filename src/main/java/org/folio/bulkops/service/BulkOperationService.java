5cb1d6237ba57443043fa206eb6ff5cc35bf5892
Mikita Siadykh <mikita_siadykh@epam.com>
2026-05-25 16:47:16 +0300

    test

--- src/main/java/org/folio/bulkops/service/BulkOperationService.java
+++ src/main/java/org/folio/bulkops/service/BulkOperationService.java
@@ -774,8 +774,10 @@ public class BulkOperationService {
                 () -> {
                   try {
                     log.info("Launching batch job");
+                    var tempDir = Path.of(getWorkDir() + operation.getId());
+                    Files.createDirectories(tempDir);
                     var tempIdentifiersFilePath =
-                        getWorkDir() + operation.getId() + SLASH + "identifiers.csv";
+                        tempDir + SLASH + "identifiers.csv";
                     try (var identifiersStream =
                         remoteFileSystemClient.get(
                             operation.getLinkToTriggeringCsvFile())) {
